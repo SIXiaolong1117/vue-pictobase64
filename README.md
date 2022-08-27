26e8b2e5ecbca8fd99d3471a6b942de464100ec8
SI_Xiaolong <sixiaolong2018@outlook.com>
2022-08-27 20:38:01 +0800

    完成移植，存在一些问题。

---
+++ README.md
@@ -0,0 +1,41 @@
+# 图片转 Base64 编码 (Vue版)
+
+个人的一个 Vue + Electron 的练手项目，可以实现便捷的将图片转换为 Base64 编码形式。
+
+原生 JS + Electron 的版本见：[PicToBase64](https://github.com/Direct5dom/PicToBase64)
+
+## 项目预览
+
+![img](./README/2022-08-15-190024.png)
+
+## ⬇下载
+
+您可以直接到[Releases · Direct5dom/vue-PicToBase64](https://github.com/Direct5dom/vue-PicToBase64/releases)下载已经打包好的软件。
+
+## 🛠️构建
+
+想要构建此项目，您需要：
+
+```sh
+git clone https://github.com/Direct5dom/vue-PicToBase64
+cd vue-PicToBase64
+yarn
+```
+
+运行
+
+```sh
+yarn electron:serve
+```
+
+## 📦︎打包
+
+想要打包此项目，您需要在项目根目录运行：
+
+```sh
+yarn electron:build
+```
+
+## ⚖️License
+
+[MIT license](https://github.com/Direct5dom/vue-PicToBase64/blob/main/LICENSE)
---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+  mounted() {
+    window.readFile = this.readFile;
+    var img_upload = document.getElementById("img_upload");
+    // 添加功能出发监听事件
+    img_upload.addEventListener('change', readFile, false);
+  },
+  methods: {
+    tirggerFile: function (event) {
+      var file = event.target.files[0]; // (利用console.log输出看结构就知道如何处理档案资料)
+      console.log(file);
+      if (!/image\/\w+/.test(file.type)) {
+        alert("请确保文件为图像类型");
+        return false;
+      }
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        var timeStart = Date.now()
+        // this.result里的这个result是FileReader.readAsDataURL()接口当中转换完图片输出的base64结果存放在result当中
+        base64_code.innerHTML = this.result;
+        img_area.innerHTML = '<img src="' + this.result + '" alt=""/>';
+        var timeEnd = Date.now()
+        var timeDiff = timeEnd - timeStart
+        console.log("Conversion time: ", timeDiff, " ms");
+        time_diff.innerHTML = '<h3 class="timeDiff">转换用时 ' + timeDiff + ' ms</h3><h4>Conversion time ' + timeDiff + ' ms</h4>';
+      }
+      // 监听元素变动，一旦有变动便运行readFile
+      img_upload.addEventListener('change', readFile, false);
+    },
+    base64Decode: function () {
+      var base64Decode = base64_input.innerHTML;
+      img_area.innerHTML = '<img src="' + base64Decode + '" alt=""/>';
+    },
+    copyCode: function () {
+      var copyText = base64_code.innerHTML;
+      console.log(copyText);
+      var content = document.getElementById('base64_code').innerHTML;
+
+      navigator.clipboard.writeText(content)
+        .then(() => {
+          console.log("Text copied to clipboard...")
+        })
+        .catch(err => {
+          console.log('Something went wrong', err);
+        })
+    }
+  },
+}
+</script>
+
+<template>
+  <h1>Pic To Base64</h1>
+  <div class="display">
+    <div class="code_div">
+      <h2>BASE64</h2>
+      <h3>Base64</h3>
+      <hr>
+      <textarea id="base64_code"></textarea>
+    </div>
+    <div class="img_div">
+      <h2>图片预览</h2>
+      <h3>Preview</h3>
+      <hr>
+      <p id="img_area"></p>
+    </div>
+  </div>
+  <nav>
+    <input type="file" id="img_upload" @change="tirggerFile($event)" />
+    <!-- <input type="file" id="img_upload" hidden="hidden" />
+			<button><label for="img_upload">上传文件</label></button> -->
+    <button type="button" @onclick="copyCode()">复制 Base64</button>
+    <button type="button" onclick="javascript:location.reload()">清空内容</button>
+  </nav>
+  <p id="time_diff"></p>
+</template>
